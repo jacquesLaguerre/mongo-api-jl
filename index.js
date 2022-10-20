3818bfa3d228f24d9cd2b5d0aecdcf2a5e30d52f
Jacques Laguerre <jacquesblaguerre@gmail.com>
2022-10-20 10:47:06 -0400

    Creating a collection furniture with mongo connected to APi

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express'
+import {addNewFurniture} from './src/furniture.js'
 
 const PORT = 3030
 const app = express()
 app.use(express.json())
+
 app.get('/', (req,res)=> res.send('working'))
+app.post('/furniture', addNewFurniture)
+
 
 
 app.listen(PORT, ()=> console.log(`listening on http://localhost${PORT}...`))
